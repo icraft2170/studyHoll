d445271cf9845502b98cbbf736d276cc4a7322e8
Hero <icraft2170@gmail.com>
2022-01-08 13:34:42 +0900

    설정파일 등

---
+++ src/test/java/me/hyeonho/studyolle/StudyHollApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hyeonho.studyolle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
