df712aa8bd7992c3f979855e73c9b91e2aa8d9b2
Hero <icraft2170@gmail.com>
2022-01-08 15:43:33 +0900

    회원가입 Validation ( Custom Validator , InitBinder 등)

---
+++ src/main/java/me/hyeonho/studyolle/account/AccountRepository.java
@@ -0,0 +1,12 @@
+package me.hyeonho.studyolle.account;
+
+import me.hyeonho.studyolle.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account,Long> {
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+}
